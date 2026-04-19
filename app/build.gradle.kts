40c50d1bf44375cdcc43c46af40e4583f1bd95d0
dotmg <mahefa@homemade.mg>
2026-04-19 17:41:56 +0200

    Continuous build requires version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
             libs.versions.targetSdk
                 .get()
                 .toInt()
+        versionCode 1
+        versionName "1.10.1"
 
         vectorDrawables.useSupportLibrary = true
 
